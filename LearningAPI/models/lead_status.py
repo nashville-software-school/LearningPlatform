773668eba385eeba9315c7d18335c8fae1d7911c
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-06-05 15:00:52 -0500

    migrated and integrated into urls

--- LearningAPI/models/lead_status.py
+++ LearningAPI/models/lead_status.py
@@ -4,7 +4,7 @@ from . import NssUser
 
 
 class LeadStatus(models.Model):
-    nss_user_id = models.ForeignKey(NssUser, on_delete=models.DO_NOTHING)
+    nss_user = models.ForeignKey(NssUser, on_delete=models.DO_NOTHING)
     # lead_id = models.ForeignKey(Lead, on_delete=models.DO_NOTHING)
     # status_type = models.ForeignKey(StatusType, on_delete=models.DO_NOTHING)
     date = models.DateField()
