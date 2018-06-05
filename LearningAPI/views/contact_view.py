13533c95332a7a53db8113313fba692a39cb3d01
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-06-05 14:20:14 -0500

    add contact view

---
+++ LearningAPI/views/contact_view.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from LearningAPI.serializers import ContactSerializer
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows contacts to be viewed or edited.
+    """
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
