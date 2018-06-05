ae97c426a75d60470e3169a7e768291f981d38fe
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-06-05 14:57:20 -0500

    add model

---
+++ LearningAPI/models/lead_status.py
@@ -0,0 +1,11 @@
+from django.db import models
+from . import NssUser
+# from . import Lead, StatusType
+
+
+class LeadStatus(models.Model):
+    nss_user_id = models.ForeignKey(NssUser, on_delete=models.DO_NOTHING)
+    # lead_id = models.ForeignKey(Lead, on_delete=models.DO_NOTHING)
+    # status_type = models.ForeignKey(StatusType, on_delete=models.DO_NOTHING)
+    date = models.DateField()
+    notes = models.TextField()
