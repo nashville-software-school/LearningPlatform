ca998fa419f80489ab83f8f8c0af360d8543edc4
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-06-06 13:58:35 -0500

    condense lead migrations

--- LearningAPI/models/lead.py
+++ LearningAPI/models/lead.py
@@ -5,7 +5,7 @@ from . import Company, Contact
 
 class Lead(models.Model):
     date = models.DateField(max_length=55)
-    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING, null=True)
-    contact = models.ForeignKey(Contact, on_delete=models.DO_NOTHING, null=True)
+    company_id = models.ForeignKey(Company, on_delete=models.DO_NOTHING, null=True)
+    contact_id = models.ForeignKey(Contact, on_delete=models.DO_NOTHING, null=True)
     notes = models.TextField()
     # lead_generation_type_id = models.ForeignKey(LeadGeneration, on_delete=models.DO_NOTHING)
