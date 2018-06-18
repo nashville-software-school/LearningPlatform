48d157c33fd5a19178ccb9c85d78f2df993e8ac2
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-06-18 11:54:25 -0500

    company-contact model

--- LearningAPI/models/company.py
+++ LearningAPI/models/company.py
@@ -1,10 +1,12 @@
 from django.db import models
+from . import Contact
 
 
 class Company(models.Model):
     name = models.CharField(max_length=55)
     phone = models.CharField(max_length=55)
     address = models.CharField(max_length=55)
+    contacts = models.ManyToManyField(Contact)
 
     def __str__(self):
         return self.name
