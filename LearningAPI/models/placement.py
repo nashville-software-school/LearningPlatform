9e7e4e3d1213acfce6ba17cfa56162b094f76195
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-06-26 11:33:56 -0500

    remove _id from foregin keys on placements

--- LearningAPI/models/placement.py
+++ LearningAPI/models/placement.py
@@ -3,9 +3,9 @@ from . import NssUser, JobType, Company, Technology
 
 
 class Placement(models.Model):
-    nss_user_id = models.ForeignKey(NssUser, on_delete=models.DO_NOTHING)
-    company_id = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
-    job_type_id = models.ForeignKey(JobType, on_delete=models.DO_NOTHING)
+    nss_user = models.ForeignKey(NssUser, on_delete=models.DO_NOTHING)
+    company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
+    job_type = models.ForeignKey(JobType, on_delete=models.DO_NOTHING)
     salary = models.IntegerField()
     start_date = models.DateField()
     technologies = models.ManyToManyField(Technology)
