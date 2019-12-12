299182bb9331c9dbd027acfbc4f0bd2688af3c54
Joe Shepherd <joeshepmedia@gmail.com>
2019-12-12 13:53:48 -0600

    update InstructorCohort to use new current_cohort calculation

--- LearningAPI/models/instructor_cohort.py
+++ LearningAPI/models/instructor_cohort.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.utils import timezone
+
 
 # No need to import the referenced models, thanks to 'lazy loading' that happens when we put the Model name in quotes. This helps avoid circular import error
 class InstructorCohort(models.Model):
     """Represents relationship between and Instructor and Cohort"""
 
-    instructor = models.ForeignKey("Instructor", on_delete=models.DO_NOTHING)
+    instructor = models.ForeignKey("Instructor", on_delete=models.DO_NOTHING, related_name="cohorts")
     cohort = models.ForeignKey("Cohort", on_delete=models.DO_NOTHING)
+    date_assigned = models.DateField(default=timezone.now, editable=False)
